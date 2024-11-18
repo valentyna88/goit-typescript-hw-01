abb30b9110071c6eba61975279171619c84fa4da
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-18 12:48:55 +0200

    Add basic tasks

---
+++ src/basic/3.ts
@@ -0,0 +1,2 @@
+let unionType: string | number;
+let status: "enable" | "disable";
---
+++ src/basic/6.ts
@@ -0,0 +1,25 @@
+interface User {
+  name: string;
+  age: number;
+  email: string;
+  address?: {
+    city: string;
+    country: string;
+  };
+}
+
+const mango = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const poly = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
